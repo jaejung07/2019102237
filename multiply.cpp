c15a3ad111c78e5d0b2b650f4128ec9583eb3b5c
chw <jipegalre@gmail.com>
2021-10-08 11:14:26 +0900

    9단 까지 나올 수 있도록 수정

--- multiply.cpp
+++ multiply.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	int dan = 8;
+	int dan = 9; //dan을 9로 수정하여 9단까지 나올 수 있도록 수정
 	for (int row = 1; row <= dan; row++) {
 		for (int col = 1; col <= dan; col++)
 			cout << setw(4) << (row * col) << " ";
